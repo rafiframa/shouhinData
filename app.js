72dfb13d77dd152ef42a456abe6f019b00c92965
unknown <rafiframa@gmail.com>
2025-05-22 13:53:52 +0900

    tes

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ async function startServer() {
     }
     
     // Start Express server
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
