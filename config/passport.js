b99b583b5d01639738df7f2764a38b838cbd0724
unknown <rafiframa@gmail.com>
2025-05-26 16:04:06 +0900

    tes2

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const { User } = require('../models');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://shouhindata.onrender.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
